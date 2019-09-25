9a3d8e5849c32550789e4bfbb45b42bb33ab8972
Your Name <beltondavid1@gmail.com>
2019-09-25 15:19:25 +0800

    Modify getting of data (halfway through)
    Multiple log in/out entries still not yet coded.
    getting of HRIS ID to query instead of userid still not yet coded also.
    modified:   app/Http/Controllers/HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,8 +13,8 @@ class HomeController extends Controller
     public function index()
     {
         // $from = date('2019-05-02');
-        $from = Carbon::createFromDate(2019, 05, 2)->format('Y-m-d');
-        $to = Carbon::createFromDate(2019, 05, 9)->format('Y-m-d');
+        $from = Carbon::createFromDate(2019, 05, 20)->format('Y-m-d');
+        $to = Carbon::createFromDate(2019, 05, 22)->format('Y-m-d');
         // $to = date('2019-05-09');
 
         // $logIn = UserInfo::SelectLogIn()->JoinCol()->HrisId(1)->where('checktime', '>=', $from)->where('checktime', '<=', $to)->TimeType('i')->OrderDate()->get();
@@ -31,120 +31,120 @@ class HomeController extends Controller
             $size = count($logIn);
         }
 
-        for ($count = 0, $x = 0, $y = 0; $count < $size; $count++, $x++, $y++) {
-            $setIn = null;
-            $setOut = null;
-            if (isset($logIn[$x]->checktime)) {
-                $setIn = 1;
-                $compareDate1 = explode(" ", $logIn[$x]->checktime);
+        for ($count = 0, $counter1 = 0, $counter2 = 0; $count < $size; $count++, $counter1++, $counter2++) {
+            if (isset($logIn[$counter1]->checktime)) {
+                $Date1 = explode(" ", $logIn[$counter1]->checktime);
             } else {
-                $setIn = 0;
-                $compareDate1 = null;
+                $Date1 = null;
             }
-            if (isset($logOut[$y]->checktime)) {
-                $setOut = 1;
-                $compareDate2 = explode(" ", $logOut[$y]->checktime);
-            } else {
-                $setOut = 0;
-                $compareDate2 = null;
-            }
-
             
-            if ($compareDate1 != null && $compareDate2 != null) {
-                if ($compareDate1[0] == $compareDate2[0]) {
+            if (isset($logOut[$counter2]->checktime)) {
+                $Date2 = explode(" ", $logOut[$counter2]->checktime);
+            } else {
+                $Date2 = null;
+            }
+            
+
+            if ($Date1 != null && $Date2 != null) {
+                if ($Date1[0] == $Date2[0]) {
                     $this->pushCollection(
                         $collection,
-                        $logIn[$x]->userid,
-                        $compareDate1[0],
-                        $logIn[$x]->checktime,
-                        $logOut[$y]->checktime,
-                        $logIn[$x]->name
+                        $logIn[$counter1]->userid,
+                        $Date1[0],
+                        $logIn[$counter1]->checktime,
+                        $logOut[$counter2]->checktime,
+                        $logIn[$counter1]->name
                     );
-                } elseif ($compareDate1[0] > $compareDate2[0]) { // EMPLOYEE DIDN'T LOG IN OR MULTIPLE LOG OUT
+                } elseif ($Date1[0] > $Date2[0]) { // EMPLOYEE DIDN'T LOG IN OR MULTIPLE LOG OUT
                     $this->pushCollection(
                         $collection,
-                        $logIn[$x]->userid,
-                        $compareDate2[0],
+                        $logIn[$counter1]->userid,
+                        $Date2[0],
                         "N/A",
-                        $logOut[$y]->checktime,
-                        $logIn[$x]->name
+                        $logOut[$counter2]->checktime,
+                        $logIn[$counter1]->name
                     );
-                    $x -= 1;
-                } elseif ($compareDate1[0] < $compareDate2[0]) { // EMPLOYEE DIDN'T LOG IN OR MULTIPLE LOG OUT
+                    $counter1 -= 1;
+                    $size += 1;
+                } elseif ($Date1[0] < $Date2[0]) { // EMPLOYEE DIDN'T LOG OUT OR MULTIPLE LOG IN
                     $this->pushCollection(
                         $collection,
-                        $logIn[$x]->userid,
-                        $compareDate1[0],
-                        $logIn[$x]->checktime,
+                        $logIn[$counter1]->userid,
+                        $Date1[0],
+                        $logIn[$counter1]->checktime,
                         "N/A",
-                        $logIn[$x]->name
+                        $logIn[$counter1]->name
                     );
-                    $y -= 1;
+                    $counter2 -= 1;
+                    $size += 1;
                 }
-            } elseif ($compareDate1 == null && $compareDate2 != null) {
+
+                // $prevLogInDate = [$Date1];
+                // $prevLogOutDate = [$Date2];
+            } elseif ($Date1 == null && $Date2 != null) {
                 $this->pushCollection(
                     $collection,
-                    $logIn[$x-1]->userid,
-                    $compareDate2[0],
+                    $logIn[$counter1-1]->userid,
+                    $Date2[0],
                     "N/A",
-                    $logOut[$y]->checktime,
-                    $logIn[$x-1]->name
+                    $logOut[$counter2]->checktime,
+                    $logIn[$counter1-1]->name
                 );
-            } elseif ($compareDate1 != null && $compareDate2 == null) {
+            } elseif ($Date1 != null && $Date2 == null) {
                 $this->pushCollection(
                     $collection,
-                    $logIn[$x]->userid,
-                    $compareDate1[0],
-                    $logIn[$x]->checktime,
+                    $logIn[$counter1]->userid,
+                    $Date1[0],
+                    $logIn[$counter1]->checktime,
                     "N/A",
-                    $logIn[$x]->name
+                    $logIn[$counter1]->name
                 );
             }
         }
-        dd($logIn, $logOut, $collection);
+        // dd($logIn, $logOut, $collection);
 
 
         /*
         for ($counter1 = 0, $counter2 = 0, $loopCounter = 0; $counter1 < count($logIn) && $counter2 < count($logOut); $counter1++, $counter2++, $loopCounter++) {
-            $compareDate1 = explode(" ", $logIn[$counter1]->checktime);
-            $compareDate2 = explode(" ", $logOut[$counter2]->checktime);
+            $Date1 = explode(" ", $logIn[$counter1]->checktime);
+            $Date2 = explode(" ", $logOut[$counter2]->checktime);
 
-            // $this->pushCollection($collection, $logIn[$counter1]->userid, $compareDate1[0], $logIn[$counter1]->checktime,
+            // $this->pushCollection($collection, $logIn[$counter1]->userid, $Date1[0], $logIn[$counter1]->checktime,
             //         $logOut[$counter2]->checktime, $logIn[$counter1]->name);
 
-            if ($compareDate1[0] == $compareDate2[0]) {
+            if ($Date1[0] == $Date2[0]) {
                 $this->pushCollection(
                     $collection,
                     $logIn[$counter1]->userid,
-                    $compareDate1[0],
+                    $Date1[0],
                     $logIn[$counter1]->checktime,
                     $logOut[$counter2]->checktime,
                     $logIn[$counter1]->name
                 );
-            } elseif ($compareDate1[0] > $compareDate2[0]) { // EMPLOYEE DIDN'T LOG IN OR MULTIPLE LOG OUT
-                // if(($compareDate2[0] == explode(" ", $logOut[$counter2-1]->checktime)[0]) && ($compareDate2[1] > explode(" ", $logOut[$counter2-1]->checktime)[1])){
+            } elseif ($Date1[0] > $Date2[0]) { // EMPLOYEE DIDN'T LOG IN OR MULTIPLE LOG OUT
+                // if(($Date2[0] == explode(" ", $logOut[$counter2-1]->checktime)[0]) && ($Date2[1] > explode(" ", $logOut[$counter2-1]->checktime)[1])){
                 //     $collection->replace([$loopCounter-1 => ['logOut' => $logOut[$counter2]->checktime]]);
                 // }
                 // else {
                 $this->pushCollection(
                         $collection,
                         $logIn[$counter1]->userid,
-                        $compareDate2[0],
+                        $Date2[0],
                         "N/A",
                         $logOut[$counter2]->checktime,
                         $logIn[$counter1]->name
                     );
                 $counter1 -= 1;
             // }
-            } elseif ($compareDate1[0] < $compareDate2[0]) { // EMPLOYEE DIDN'T LOG OUT OR MULTIPLE LOG OUT
-                // if(($compareDate1[0] == explode(" ", $logIn[$counter1-1]->checktime)[0]) && ($compareDate1[1] > explode(" ", $logIn[$counter1-1]->checktime)[1])){
+            } elseif ($Date1[0] < $Date2[0]) { // EMPLOYEE DIDN'T LOG OUT OR MULTIPLE LOG IN
+                // if(($Date1[0] == explode(" ", $logIn[$counter1-1]->checktime)[0]) && ($Date1[1] > explode(" ", $logIn[$counter1-1]->checktime)[1])){
                 //     // DO NOTHING AND DONT PUSH
                 // }
                 // else {
                     $this->pushCollection(
                         $collection,
                         $logIn[$counter1]->userid,
-                        $compareDate1[0],
+                        $Date1[0],
                         $logIn[$counter1]->checktime,
                         "N/A",
                         $logIn[$counter1]->name
@@ -166,50 +166,82 @@ class HomeController extends Controller
 
         $collection = collect([]);
 
-        for ($counter1 = 0, $counter2 = 0, $loopCounter = 0; ($counter1 < count($logIn)) && ($counter2 < count($logOut)); $counter1++, $counter2++, $loopCounter++) {
-            $compareDate1 = explode(" ", $logIn[$counter1]->checktime);
-            $compareDate2 = explode(" ", $logOut[$counter2]->checktime);
-            
-            // $this->pushCollection(
-            //     $collection,
-            //     $logIn[$counter1]->userid,
-            //     $compareDate1[0],
-            //     $logIn[$counter1]->checktime,
-            //     $logOut[$counter2]->checktime,
-            //     $logIn[$counter1]->name
-            // );
+        if (count($logIn) < count($logOut)) {
+            $size = count($logOut);
+        } elseif (count($logIn) > count($logOut)) {
+            $size = count($logIn);
+        } else {
+            $size = count($logIn);
+        }
 
-            if ($compareDate1[0] == $compareDate2[0]) {
-                $this->pushCollection(
-                    $collection,
-                    $logIn[$counter1]->userid,
-                    $compareDate1[0],
-                    explode(" ", $logIn[$counter1]->checktime)[1],
-                    explode(" ", $logOut[$counter2]->checktime)[1],
-                    $logIn[$counter1]->name
-                );
-            } elseif ($compareDate1[0] > $compareDate2[0]) { // EMPLOYEE DIDN'T LOG IN OR MULTIPLE LOG OUT
-                $this->pushCollection(
-                    $collection,
-                    $logIn[$counter1]->userid,
-                    $compareDate2[0],
-                    "N/A",
-                    explode(" ", $logOut[$counter2]->checktime)[1],
-                    $logIn[$counter1]->name
-                );
-                $counter1 -= 1;
-            // }
-            } elseif ($compareDate1[0] < $compareDate2[0]) { // EMPLOYEE DIDN'T LOG OUT OR MULTIPLE LOG OUT
+        for ($count = 0, $counter1 = 0, $counter2 = 0; $count < $size; $count++, $counter1++, $counter2++) {
+            if (isset($logIn[$counter1]->checktime)) {
+                $Date1 = explode(" ", $logIn[$counter1]->checktime);
+            } else {
+                $Date1 = null;
+            }
+            
+            if (isset($logOut[$counter2]->checktime)) {
+                $Date2 = explode(" ", $logOut[$counter2]->checktime);
+            } else {
+                $Date2 = null;
+            }
+            
+
+            if ($Date1 != null && $Date2 != null) {
+                if ($Date1[0] == $Date2[0]) {
                     $this->pushCollection(
                         $collection,
                         $logIn[$counter1]->userid,
-                        $compareDate1[0],
-                        explode(" ", $logIn[$counter1]->checktime)[1],
+                        $Date1[0],
+                        $logIn[$counter1]->checktime,
+                        $logOut[$counter2]->checktime,
+                        $logIn[$counter1]->name
+                    );
+                } elseif ($Date1[0] > $Date2[0]) { // EMPLOYEE DIDN'T LOG IN OR MULTIPLE LOG OUT
+                    $this->pushCollection(
+                        $collection,
+                        $logIn[$counter1]->userid,
+                        $Date2[0],
+                        "N/A",
+                        $logOut[$counter2]->checktime,
+                        $logIn[$counter1]->name
+                    );
+                    $counter1 -= 1;
+                    $size += 1;
+                } elseif ($Date1[0] < $Date2[0]) { // EMPLOYEE DIDN'T LOG OUT OR MULTIPLE LOG IN
+                    $this->pushCollection(
+                        $collection,
+                        $logIn[$counter1]->userid,
+                        $Date1[0],
+                        $logIn[$counter1]->checktime,
                         "N/A",
                         $logIn[$counter1]->name
                     );
-                $counter2 -= 1;
-                // }
+                    $counter2 -= 1;
+                    $size += 1;
+                }
+
+                // $prevLogInDate = [$Date1];
+                // $prevLogOutDate = [$Date2];
+            } elseif ($Date1 == null && $Date2 != null) { // IF DATE 1 IS MISSING
+                $this->pushCollection(
+                    $collection,
+                    $logIn[$counter1-1]->userid,
+                    $Date2[0],
+                    "N/A",
+                    $logOut[$counter2]->checktime,
+                    $logIn[$counter1-1]->name
+                );
+            } elseif ($Date1 != null && $Date2 == null) { // IF DATE 2 IS MISSING
+                $this->pushCollection(
+                    $collection,
+                    $logIn[$counter1]->userid,
+                    $Date1[0],
+                    $logIn[$counter1]->checktime,
+                    "N/A",
+                    $logIn[$counter1]->name
+                );
             }
         }
     
