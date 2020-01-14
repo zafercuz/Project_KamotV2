ad5c9a3d9944b526f69d30724f62007e3a938336
Steven <beltondavid1@gmail.com>
2020-01-14 08:57:00 +0800

    Log out other devices still not working NABUANG
    modified:   app/Http/Controllers/Auth/LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -22,32 +22,6 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    public function login(Request $request)
-    {
-        $this->validate($request, [
-            'login'    => 'required',
-            'password' => 'required',
-        ]);
-
-        $login_type = filter_var($request->input('login'), FILTER_VALIDATE_EMAIL ) 
-            ? 'email' 
-            : 'hrisid';
-
-        $request->merge([
-            $login_type => $request->input('login')
-        ]);
-
-        if (Auth::attempt($request->only($login_type, 'password'))) {
-            return redirect()->intended($this->redirectPath());
-        }
-
-        return redirect()->back()
-            ->withInput()
-            ->withErrors([
-                'login' => 'These credentials do not match our records.',
-            ]);
-    }
-
     /**
      * Where to redirect users after login.
      *
@@ -64,4 +38,45 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated()
+    {
+        Auth::logoutOtherDevices(request('password'));
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'login'    => 'required',
+            'password' => 'required',
+        ]);
+
+        $login_type = filter_var($request->input('login'), FILTER_VALIDATE_EMAIL ) 
+            ? 'email' 
+            : 'hrisid';
+
+        $request->merge([
+            $login_type => $request->input('login')
+        ]);
+
+        if (Auth::attempt($request->only($login_type, 'password'))) {
+            $this->authenticated();
+
+            return redirect()->intended($this->redirectPath());
+        }
+
+        return redirect()->back()
+            ->withInput()
+            ->withErrors([
+                'login' => 'These credentials do not match our records.',
+            ]);
+    }
+
 }
